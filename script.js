031e1e4f4193cb0e52667b284b504cd082730c4c
Ldigah <ldigah4@gmail.com>
2019-02-13 14:42:01 +0200

    Added clear button with size prompt

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 const gridContainer = document.querySelector('#gridContainer');
+const button = document.querySelector('#clearButton');
 
+button.addEventListener('click', (e) => {
+    reset();
+})
 
-
-function createCells(number) {
-    number = 16 * 16;
-    for (let i = 0; i < number; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.textContent = 'Cell';
-        cell.style.backgroundColor = 'aqua';
-        gridContainer.appendChild(cell);
+function createGrid(size) {
+    for (let row=0; row<size; row++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add("gridRow");
+        gridRow.style.height = (100 / size) + "%";
+        for (let column=0; column<size; column++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            grid.style.width = (100 / size) + '%';
+            gridRow.appendChild(grid);
+        }
+        gridContainer.appendChild(gridRow);
     }
+    changeColor();
 }
 
-createCells();
+function getRandomColor() {
+    var color = Math.floor(Math.random() * 16777216).toString(16);
+    return '#000000'.slice(0, -color.length) + color;
+}
+
+function changeColor() {
+    let cell = document.querySelectorAll(".grid");
+    cell.forEach(cel => {
+       cel.addEventListener("mouseover", (e) => {
+          e.target.style.backgroundColor = getRandomColor();
+       })
+    });
+ }
+
+ function reset() {
+    let children = document.querySelectorAll(".gridRow");
+    children.forEach(child => {
+       gridContainer.removeChild(child);
+    })
+    let getGridSize = prompt("Chose a grid size", "16");
+    createGrid(getGridSize);
+ }
+
+ createGrid(16);
